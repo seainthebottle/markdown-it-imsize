573b7c56efede1038797677a54425634aba9715b
seainthebottle <seainthebottle@gmail.com>
2022-09-18 12:06:07 +0900

    Permit whitespaces between size

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 var path = require('path');
-var webpack = require('webpack');
+//var webpack = require('webpack');
 
 module.exports = {
+  mode: "production",
+  
+  entry: "./lib/index.js",
+
   output: {
     path: __dirname + "/dist",
     filename: "markdown-it-imsize.js",
@@ -9,7 +13,12 @@ module.exports = {
     libraryTarget: "umd"
   },
 
-  node: {
-    fs: 'empty'
+  resolve: {
+    modules: [__dirname + "/lib", "node_modules"], 
+    fallback: {
+        "fs": false,
+        "path": false ,
+        "os": false
+    }
   }
 };
